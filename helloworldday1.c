4634867ae3de45749aae900d7c6f0f252d26e0b7
ziazee <43564428+ziazee@users.noreply.github.com>
2018-09-25 12:48:59 +0530

    Create helloworldday1.c

---
+++ helloworldday1.c
@@ -0,0 +1,29 @@
+1)Write a program to print hello world?
+#include<stdio.h>
+void main()
+{
+printf("\n hello world");
+}
+
+2)Write a program to print your name?
+#include<stdio.h.
+int main()
+{
+printf("\n zia ul hussain");
+return o;
+}
+
+3)What is #include<stdio.h>
+It is a preprocessor instructor which include various input and output operations which program might use it.
+
+4)What is printf?
+It is a function used to print characters,functions,srings,integers etc.
+
+5)What is main?
+It is a starting point of execution in c for any program.
+
+6)Why should you learn touch typing on qwerty keyboard?
+It elliminates the silly mistakes and saves time while coding so that the programmer can concentrate on coding rather on characters.
+
+7)How will you learn touch typing in qwerty keyboard?
+Practice and habbit. 
